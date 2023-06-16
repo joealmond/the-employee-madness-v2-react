88a621576cde2a85332800dcfbaa96097f65ba99
joealmond <jozsef.mandula@gmail.com>
2023-06-16 20:13:14 +0200

    added filterByPlayers based on query parameter

--- client/src/Pages/GameList.jsx
+++ client/src/Pages/GameList.jsx
@@ -6,9 +6,12 @@ import { useParams, useNavigate, useSearchParams } from "react-router-dom";
 const fetchGames = () => {
   return fetch("/api/games").then((res) => res.json());
 };
-const fetchMaxPlayers = (maxPlayers) => {
-  console.log(maxPlayers)
-  // return fetch(`${maxPlayers}`).then((res) => res.json());
+
+const filterByPlayers = (maxPlayers, games) => {
+  if (maxPlayers) {
+    return games.filter(game => game.maxPlayers <= maxPlayers);
+  }
+  return games;
 };
 
 const deleteGame = (id) => {
@@ -25,27 +28,27 @@ const GameList = () => {
 
   const handleDelete = (id) => {
     deleteGame(id);
-
     setGames((game) => {
       return game.filter((game) => game._id !== id);
     });
   };
 
   useEffect(() => {
-    fetchMaxPlayers(maxPlayers)
     fetchGames()
-      .then((game) => {
+      .then((games) => {
         setLoading(false);
-        setGames(game);
-      })
-  }, [maxPlayers]);
+        setGames(games);
+      });
+  }, []);
 
+  const filteredGames = games && filterByPlayers(maxPlayers, [...games])
 
   if (loading) {
     return <Loading />;
   }
 
-  return <GameTable games={games} onDelete={handleDelete} />;
+  return <GameTable games={filteredGames} onDelete={handleDelete} />;
 };
 
 export default GameList;
+

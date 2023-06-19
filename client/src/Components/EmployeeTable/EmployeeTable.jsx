ce53facac2be82f9c323ed3856ab801d5b29646d
joealmond <jozsef.mandula@gmail.com>
2023-06-19 18:35:11 +0200

    done - PAP1

--- client/src/Components/EmployeeTable/EmployeeTable.jsx
+++ client/src/Components/EmployeeTable/EmployeeTable.jsx
@@ -1,36 +1,58 @@
 import { Link } from "react-router-dom";
 import "./EmployeeTable.css";
+import { useState } from "react";
 
-const EmployeeTable = ({ employees, onDelete }) => (
-  <div className="EmployeeTable">
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Level</th>
-          <th>Position</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {employees.map((employee) => (
-          <tr key={employee._id}>
-            <td>{employee.name}</td>
-            <td>{employee.level}</td>
-            <td>{employee.position}</td>
-            <td>
-              <Link to={`/update/${employee._id}`}>
-                <button type="button">Update</button>
-              </Link>
-              <button type="button" onClick={() => onDelete(employee._id)}>
-                Delete
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+const EmployeeTable = ({ employees, onDelete }) => {
+  const [isDisplayingConfirm, setIsDisplayingConfirm] = useState(false);
+  const [employeeId, setEmployeeId] = useState(null);
+
+  const onDeleteClick = (employeeId) => {
+    setIsDisplayingConfirm(!isDisplayingConfirm)
+    setEmployeeId(employeeId)
+  };
+
+  return (
+    <>
+    {isDisplayingConfirm && <dialog open>
+        <p>Are you sure?</p>
+          <button onClick={()=>{onDelete(employeeId); setIsDisplayingConfirm(false)}} >Yes</button>
+          <button onClick={()=>setIsDisplayingConfirm(false)} >No</button>
+      </dialog>}
+      
+      <div className="EmployeeTable">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Level</th>
+              <th>Position</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((employee) => (
+              <tr key={employee._id}>
+                <td>{employee.name}</td>
+                <td>{employee.level}</td>
+                <td>{employee.position}</td>
+                <td>
+                  <Link to={`/update/${employee._id}`}>
+                    <button type="button">Update</button>
+                  </Link>
+                  <button
+                    type="button"
+                    onClick={() => onDeleteClick(employee._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
 export default EmployeeTable;

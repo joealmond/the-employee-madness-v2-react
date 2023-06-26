951baaf0a9e91fd68edb97f261c8f40dafc9b9b2
joealmond <jozsef.mandula@gmail.com>
2023-06-26 14:02:30 +0200

    fixed bug in controlled inputs

--- client/src/Pages/WorklogCreator.jsx
+++ client/src/Pages/WorklogCreator.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 const WorklogCreator = () => {
@@ -7,33 +7,35 @@ const WorklogCreator = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
+  const createWorklog = async (id) => {
+    try {
+      const res = await fetch(`/api/employees/${id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          label,
+          hours,
+        }),
+      });
+
+      if (res.ok) {
+        console.log("Worklog added!");
+        setLabel("");
+        setHours("");
+        navigate("/");
+      } else {
+        console.error("Failed to create worklog.");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error.message);
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(hours, label);
-    const createWorklog = async () => {
-      try {
-        const res = await fetch(`/api/employees/${id}`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            label,
-            hours,
-          }),
-        });
-
-        if (res.ok) {
-          navigate("/");
-        } else {
-          console.error("Failed to create worklog.");
-        }
-      } catch (error) {
-        console.error("An error occurred:", error.message);
-      }
-    };
-
-    createWorklog();
+    if (label !== "" && hours !== "") createWorklog(id);
   };
 
   return (
@@ -41,11 +43,19 @@ const WorklogCreator = () => {
       <h3>Add Worklog:</h3>
       <label>
         Label for work:
-        <input type="text" onChange={(e) => setLabel(e.target.value)} />
+        <input
+          type="text"
+          value={label}
+          onChange={(e) => setLabel(e.target.value)}
+        />
       </label>
       <label>
         Hours to log:
-        <input type="number" onChange={(e) => setHours(e.target.value)} />
+        <input
+          type="number"
+          value={hours}
+          onChange={(e) => setHours(e.target.value)}
+        />
       </label>
       <label>
         <input type="Submit" />
